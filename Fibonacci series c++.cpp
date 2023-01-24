34125804f824e21ea2663fd3daaa4a2581139164
hasinipunugoti412 <114474733+hasinipunugoti412@users.noreply.github.com>
2023-01-24 13:38:30 +0530

    Add files via upload

---
+++ Fibonacci series c++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1=0,n2=1,n3,num,i;
+	cout<<"Enter the number= ";
+	cin>>num;
+	cout<<n1<<" "<<n2<<" ";
+	for(i=2;i<num;i++)
+	{
+		n3=n1+n2;
+		cout<<n3<<" ";
+		n1=n2;
+		n2=n3;
+	}
+	return 0;
+}
