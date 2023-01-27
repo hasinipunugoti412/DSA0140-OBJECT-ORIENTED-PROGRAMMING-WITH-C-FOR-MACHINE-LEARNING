161986be92c8345011994e2cf9b3a38d4a8c328a
hasinipunugoti412 <114474733+hasinipunugoti412@users.noreply.github.com>
2023-01-27 13:56:03 +0530

    Add files via upload

---
+++ Square and cube of number c++.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double s,c;
+	float n;
+	cout<<"Enter the number= ";
+	cin>>n;
+	s=n*n;
+	c=n*n*n;
+	cout<<"\nSquare Number= "<<s;
+	cout<<"\nCube Number= "<<c;
+	return 0;
+}
